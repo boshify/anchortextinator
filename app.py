fc6ab91acf0d5e958419eac447cb0287164f6ca5
boshify <140970186+boshify@users.noreply.github.com>
2023-09-18 23:05:27 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import openai
+
+def get_body_text(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.3'}
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return ' '.join([p.text for p in soup.find_all('p')])
+    except Exception as e:
+        st.warning(f"Failed to crawl {url}. Error: {e}")
+        return None
+
+def get_recommendations(body_text, target_keyword):
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=f"Find the best placements in the text for the keyword '{target_keyword}' linking to a destination page.\n\nText: {body_text}\n",
+        max_tokens=200,
+        n=3,
+        stop=["Text:"]
+    )
+    
+    recommendations = [choice['text'].strip() for choice in response.choices]
+    return recommendations
+
+def main():
+    st.title('The Anchor Textinator')
+
+    destination_url = st.text_input("Enter destination page URL")
+    source_urls = st.text_area("Enter list of source URLs (one per line)")
+    target_keyword = st.text_input("Enter target keyword")
+
+    if st.button('Get Recommendations'):
+        urls = [url.strip() for url in source_urls.split('\n') if url.strip()]
+        
+        for url in urls:
+            body_text = get_body_text(url)
+            if body_text:
+                recommendations = get_recommendations(body_text, target_keyword)
+                st.subheader(f"Recommendations for {url}")
+                for idx, rec in enumerate(recommendations, 1):
+                    st.text_box(f"Option {idx}", rec)
+
+if __name__ == "__main__":
+    main()
