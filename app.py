3f4bf8af31c97a5bc412d4a8abb9cc497f2a2111
boshify <140970186+boshify@users.noreply.github.com>
2023-10-14 21:41:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,23 +4,24 @@ from bs4 import BeautifulSoup
 import openai
 
 def get_body_text(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.3'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.3'
+    }
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, timeout=10)  # Added timeout
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
         # Extracting text from p, div, and span tags
         return ' '.join([tag.text for tag in soup.find_all(['p', 'div', 'span'])])
-    except Exception as e:
+    except requests.RequestException as e:  # Modified exception type
         st.warning(f"Failed to crawl {url}. Error: {e}")
         return None
-
 
 def get_recommendations(body_text, target_keyword, destination_url):
     openai.api_key = st.secrets["OPENAI_API_KEY"]
 
     prompt = (f"Given the text, where would you suggest adding a link with the anchor text '{target_keyword}' pointing to '{destination_url}'? "
-              "Please specify if the keyword can be directly linked or if a modification to the sentence is needed.\n\nText: {body_text}\n")
+              f"Please specify if the keyword can be directly linked or if a modification to the sentence is needed.\n\nText: {body_text}\n")
     
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
